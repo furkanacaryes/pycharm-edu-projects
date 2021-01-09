fc6cca0d80c440f23b0b3533b8a85614e9919739
Furkan Acar <furkan@acar.digital>
2021-01-10 00:35:04 +0300

    Updated assert fn. Added comments.

--- tic-tac-toe/utils.py
+++ tic-tac-toe/utils.py
@@ -1,19 +1,24 @@
 def list_to_str(split_string: list) -> str:
+    """Converts given list to string"""
     return str().join(split_string)
 
 
-def str_to_int_list(user_coordinates: str) -> list[int]:
-    return list(map(int, user_coordinates.split()))
+def str_to_int_list(text: str) -> list[int]:
+    """Converts spaced numerics to a list[int]"""
+    return list(map(int, text.split()))
 
 
-def filter_match(_list, match):
-    return list(filter(lambda item: item == match, _list))
+def filter_match(match, collection: list) -> list:
+    """Filters exact matched items of the given list"""
+    return list(filter(lambda item: item == match, collection))
 
 
-def assert_every(_list, fn):
-    return len(list(filter(fn, _list))) == len(_list)
+def assert_every(predictable, collection: list) -> bool:
+    """Asserts every item in given list by given prediction"""
+    return all(list(map(predictable, collection)))
 
 
 def print_row_list(rows):
+    """Just prints given list[str]"""
     for row in rows:
         print(row)
